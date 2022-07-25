e7633b1caa00e3330685328ce7f207145a849461
Luis Vicuna <89670323+luis-vicuna@users.noreply.github.com>
2022-07-25 18:54:47 -0400

    Add files via upload

--- project3.js
+++ project3.js
@@ -1,54 +1,69 @@
-const randomnum = Math.floor(Math.random()*100);
-
 const uInput = document.querySelector('.uInput');
 const msg = document.querySelector('.msg');
+const guessbox = document.querySelector('.guessbox');
 const check = document.querySelector('.check');
 const score = document.querySelector('.score');
 const Hscore = document.querySelector('.Hscore');
 const reset = document.querySelector('.reset');
+const BigBox = document.querySelector('.BigBox');
+const randomnum = Math.floor(Math.random()*100);
+const guessed_nums =[];
+const pic = document.getElementById('pic');
 
-var msg3 = document.getElementById("message3");
-var guessed_nums = [];
-let scoreval = 10;
-let Hscoreval = 0;
 
-function wrong(){
-    scoreval --;
-    score.textContent =`Score: ${scoreval}`;
-    if (randomnum < uInput.value){
-        msg.textContent='Your number is too high';
-        guessed_nums.push(uInput);
-        msg3.textContent = "Guessed numbers are: " + guessed_nums;
-    }
-    else if(isNaN(uInput)){
-        output.innerHTML="That's not a number!";
-        guessed_nums.push(uInput);
-        msg3.textContent = "Guessed numbers are: " + guessed_nums;
-    }
-    else{
-        msg.textContent='Your number is too low';
-        guessed_nums.push(uInput);
-        msg3.textContent = "Guessed numbers are: " + guessed_nums;
-    }
-}
+let scorepoint = 10;
+let Hscorepoint = 0;
+
+
 console.log(randomnum);
 
 check.addEventListener('click', function() {
-
-    if(scoreval>0){
+  // Is it a number??
+  if(isNaN(uInput.value)||uInput.value ===""){
+    msg.textContent = 'Not a Number'
+  }
+  else{// It is a number can play
+    // Has points to play
+    if(scorepoint>0){
+        //Correct Number
         if (Number(uInput.value) === randomnum){
+            Hscorepoint = scorepoint;
+            Hscore.textContent =`High Score: ${Hscorepoint}`;
             msg.textContent = 'Congrats! You win!';
+            BigBox.style.background = "#228B22";
         }
+        //Wrong number
         else{
+            guessed_nums.push(uInput.value);
             wrong();
+            guessbox.textContent = guessed_nums;
         }
     }
-   
+    // No points left to play
+    else {
+      BigBox.style.background = "#DC143C";
+      pic.src="./Images/game-over.png";
+      msg.textContent= "YOU LOST";
+    }
+  }
 })
+
+function wrong(){
+    scorepoint --;
+    score.textContent =`Score: ${scorepoint}`;
+    if (randomnum < uInput.value){
+        msg.textContent='Your number is too high';
+    }
+    else{
+        msg.textContent='Your number is too low';
+    }
+}
 
 reset.addEventListener('click', function() {
 
-    scoreval = 10;
-    score.textContent =`Score: ${scoreval}`;
+    scorepoint = 10;
+    score.textContent =`Score: ${scorepoint}`;
+    BigBox.style.background = "black";
+
 
 })
