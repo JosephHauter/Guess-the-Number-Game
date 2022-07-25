8b964324d99445a80d88d07d127f21347e8c65e7
Joseph <58018853+JosephHauter@users.noreply.github.com>
2022-07-25 13:10:23 -0400

    added loops and counter
    added to check if its a number, also added the array to get displayed on site of guessed numbers

--- project3.js
+++ project3.js
@@ -7,7 +7,8 @@ const score = document.querySelector('.score');
 const Hscore = document.querySelector('.Hscore');
 const reset = document.querySelector('.reset');
 
-
+var msg3 = document.getElementById("message3");
+var guessed_nums = [];
 let scoreval = 10;
 let Hscoreval = 0;
 
@@ -16,9 +17,18 @@ function wrong(){
     score.textContent =`Score: ${scoreval}`;
     if (randomnum < uInput.value){
         msg.textContent='Your number is too high';
+        guessed_nums.push(uInput);
+        msg3.textContent = "Guessed numbers are: " + guessed_nums;
+    }
+    else if(isNaN(uInput)){
+        output.innerHTML="That's not a number!";
+        guessed_nums.push(uInput);
+        msg3.textContent = "Guessed numbers are: " + guessed_nums;
     }
     else{
         msg.textContent='Your number is too low';
+        guessed_nums.push(uInput);
+        msg3.textContent = "Guessed numbers are: " + guessed_nums;
     }
 }
 console.log(randomnum);
