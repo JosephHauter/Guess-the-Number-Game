f4e031274dfdde6d36bfac0a956fbbbafaca1484
Luis Vicuna <89670323+luis-vicuna@users.noreply.github.com>
2022-07-25 22:50:12 -0400

    Add files via upload

--- project3.js
+++ project3.js
@@ -1,13 +1,19 @@
 const uInput = document.querySelector('.uInput');
 const msg = document.querySelector('.msg');
+const msg1 = document.querySelector('.msg1');
+const msg2 = document.querySelector('.msg2');
+const msg3 = document.querySelector('.msg3');
+const side1 = document.querySelector('.side1');
+const side2 = document.querySelector('.side2');
 const guessbox = document.querySelector('.guessbox');
 const check = document.querySelector('.check');
 const score = document.querySelector('.score');
 const Hscore = document.querySelector('.Hscore');
 const reset = document.querySelector('.reset');
 const BigBox = document.querySelector('.BigBox');
-const randomnum = Math.floor(Math.random()*100);
-const guessed_nums =[];
+const trys = document.querySelector('.trys');
+let randomnum = Math.floor(Math.random()*100);
+let guessed_nums =[];
 const pic = document.getElementById('pic');
 
 
@@ -27,10 +33,26 @@ check.addEventListener('click', function() {
     if(scorepoint>0){
         //Correct Number
         if (Number(uInput.value) === randomnum){
-            Hscorepoint = scorepoint;
+            if(scorepoint>=Hscorepoint){
+              Hscorepoint = scorepoint;
+            }
             Hscore.textContent =`High Score: ${Hscorepoint}`;
             msg.textContent = 'Congrats! You win!';
             BigBox.style.background = "#228B22";
+            BigBox.style.border = "5px solid white";
+            pic.src="./Images/trophy.png";
+            uInput.style.display = "none";
+            check.style.display = "none";
+            side1.style.display = "none";
+            side2.style.display = "none";
+            msg1.textContent= `${randomnum} is the secret number`;
+            msg2.textContent= `Your Score: ${scorepoint}`;
+            msg3.textContent = `High Score: ${Hscorepoint} `
+            msg1.style.display = "block";
+            msg2.style.display = "block";
+            msg3.style.display = "block";
+            trys.style.display = "inline";
+
         }
         //Wrong number
         else{
@@ -44,6 +66,19 @@ check.addEventListener('click', function() {
       BigBox.style.background = "#DC143C";
       pic.src="./Images/game-over.png";
       msg.textContent= "YOU LOST";
+      msg1.textContent= `Don't give up!`;
+      msg2.textContent= `${randomnum} was the secret number`;
+      BigBox.style.border = "5px solid yellow";
+      // Show
+      msg1.style.display = "block";
+      msg2.style.display = "block";
+      trys.style.display = "inline";
+      // Do not show
+      uInput.style.display = "none";
+      check.style.display = "none";
+      msg3.style.display = "none";
+      side2.style.display = "none";
+
     }
   }
 })
@@ -59,11 +94,28 @@ function wrong(){
     }
 }
 
-reset.addEventListener('click', function() {
+reset.addEventListener('click', origin);
+trys.addEventListener('click', origin);
 
+function origin(){
     scorepoint = 10;
+    guessed_nums = [];
+    guessbox.textContent = '';
+    randomnum = Math.floor(Math.random()*100);
     score.textContent =`Score: ${scorepoint}`;
     BigBox.style.background = "black";
-
-
-})
+    pic.src="./Images/question.png";
+    msg.textContent='Guess a Number';
+    uInput.style.display = "inline";
+    check.style.display = "inline";
+    BigBox.style.border = "5px solid #db3eb1";
+    // Reset textContent
+    msg1.style.display = "none";
+    msg2.style.display = "none";
+    msg3.style.display = "none";
+    trys.style.display = "none";
+    // show
+    side1.style.display = "inline";
+    side2.style.display = "inline";
+    console.log(randomnum);
+}
